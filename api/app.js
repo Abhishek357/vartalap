4fbe445ca3264c8ba77c6c39c4f2e831c33d9f6d
Shikhar15606 <shikharshukla15606@gmail.com>
2021-12-11 15:17:12 +0530

    Basic API

---
+++ api/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const puppeteer = require('puppeteer');
+const template = require('./template');
+// const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes 
+const printPdf = async (req,res) => {
+    try{
+        console.log('Starting: Generating PDF Process, Kindly wait ..');
+        /** Launch a headleass browser */
+        const browser = await puppeteer.launch();
+        /* 1- Ccreate a newPage() object. It is created in default browser context. */
+        const page = await browser.newPage();
+        /* 2- Will open our generated `.html` file in the new Page instance. */
+        await page.setContent(template(req.body))
+
+        let pdf = await page.pdf({
+            format: "Tabloid",
+            margin: {
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px'
+            }
+        });
+
+        /* 4- Cleanup: close browser. */
+        await browser.close();
+        console.log('Ending: Generating PDF Process');
+        return pdf;
+    }
+    catch(error){
+        res.json({err:"Some error occured"})
+    }
+};
+
+const init = async (req,res) => {
+	try {
+		const pdf = await printPdf(req,res);
+		res.set({'Content-Type': 'application/pdf', 'Content-Length': pdf.length})
+        res.send(pdf)
+        // fs.writeFileSync('notes.pdf', pdf);
+        // console.log('Success');
+        // res.sendFile(`${__dirname}/notes.pdf`);
+        // res.json({msg:"Pdf generated successfully"});
+
+	} catch (error) {
+        console.log('Error generating PDF', error);
+        res.json({err:error})
+	}
+};
+
+
+app.post('/create-pdf', init);
+
+// app.get('/fetch-pdf', (req,res) => {
+//     res.sendFile(`${__dirname}/notes.pdf`);
+// });
+
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ api/template.js
@@ -0,0 +1,22 @@
+module.exports = (meetingData) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>${meetingData.title}</title>
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold">
+            ${meetingData.topic}
+        </h1>
+        <p class="text-md mt-2">
+            ${meetingData.description}
+        </p>
+    </body>
+    </html>
+    `;
+};
